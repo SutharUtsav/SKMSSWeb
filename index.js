4e5cf5dc63635e1c3960544fc97e82903b7470d6
csutsavsuthar <utsav.suthar@confidosoft.in>
2023-07-18 17:46:48 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+
+const PORT = process.env.PORT || 3030
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static("frontend/build"));
+    const path = require("path")
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+    })
+}
+
+app.listen(PORT,()=>{
+    console.log(`Server is listening on ${PORT}`)
+})
