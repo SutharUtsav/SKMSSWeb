ca294f93902effd3ac695672cc67694c01433ca6
csutsavsuthar <utsav.suthar@confidosoft.in>
2023-08-02 19:28:15 +0530

    Change js to ts

---
+++ dist/index.js
@@ -0,0 +1,17 @@
+"use strict";
+const express = require('express');
+const app = express();
+// const { admin, router } = require('./src/admin');
+const PORT = process.env.PORT || 3030;
+// app.use(admin.options.rootPath, router);
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("frontend/build"));
+    const path = require("path");
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
+app.listen(PORT, () => {
+    console.log(`Server is listening on ${PORT}`);
+});
+// admin.start();
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 const express = require('express')
 const app = express()
 
-const PORT = process.env.PORT || 3030
+// const { admin, router } = require('./src/admin');
+
+const PORT = process.env['PORT'] ?? 3030
+
+// app.use(admin.options.rootPath, router);
 
 
-if(process.env.NODE_ENV === "production"){
+if(process.env['NODE_ENV'] === "production"){
     app.use(express.static("frontend/build"));
     const path = require("path")
-    app.get("*",(req,res)=>{
+    app.get("*",(req:any,res:any)=>{
         res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
     })
 }
@@ -15,3 +19,5 @@ if(process.env.NODE_ENV === "production"){
 app.listen(PORT,()=>{
     console.log(`Server is listening on ${PORT}`)
 })
+
+// admin.start();
