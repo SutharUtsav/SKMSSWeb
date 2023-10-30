009e74236250e79ebed29dbac4dd7f48e1e580c4
csutsavsuthar <utsav.suthar@confidosoft.in>
2023-10-30 19:12:14 +0530

    frontend : modal for UserProfile image

--- frontend/src/admin/admin/user/UserDetails.jsx
+++ frontend/src/admin/admin/user/UserDetails.jsx
@@ -1,11 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
 import { getByQueryParams } from "../../../service/api-service";
-import UserImage from '../../../images/profile.jpg';
-import { BiEditAlt } from 'react-icons/bi'
+import UserImage from "../../../images/profile.jpg";
+import { BiEditAlt } from "react-icons/bi";
+import UserProfileImageModal from "./UserProfileImageModal";
 
 const UserDetails = () => {
-
   const params = useParams();
   const userId = params.id;
 
@@ -15,9 +15,9 @@ const UserDetails = () => {
   useEffect(() => {
     if (userId) {
       const queryJson = {
-        userId: userId
-      }
-      getByQueryParams('/user-profile/profile-image', queryJson)
+        userId: userId,
+      };
+      getByQueryParams("/user-profile/profile-image", queryJson)
         .then((response) => {
           if (response.status === 1) {
             setuserProfileImage(response.data.data);
@@ -25,71 +25,191 @@ const UserDetails = () => {
           console.log(response.data);
         })
         .catch((error) => {
-          console.log(error)
-        })
-
+          console.log(error);
+        });
 
       getByQueryParams("/user-profile", queryJson)
         .then((response) => {
           if (response.data.status === 1) {
-            setuserProfile(response.data.data)
+            setuserProfile(response.data.data);
           }
-          console.log(response.data)
+          console.log(response.data);
         })
         .catch((error) => {
           console.log(error);
-        })
+        });
     }
-  }, [userId])
+  }, [userId]);
 
   return (
-    <div className='user-details p-5 m-4 d-flex flex-column justify-content-center align-items-center'>
+    <div className="user-details p-5 m-4 d-flex flex-column justify-content-center align-items-center">
+      <div className="user-image shadow">
+        <img src={UserImage} alt="user-profile" />
+        <button title="Upload Image" className="edit-btn" data-bs-toggle="modal" data-bs-target="#uploadProfileImageModal">
+          <BiEditAlt size={"15px"} fill="#fff"  />
+        </button>
+      </div>
 
-      {
-        userProfileImage ? (
-          <div className='user-image shadow'>
-            <div>
-              <img src={UserImage} alt='user-image' />
-              <button title='Upload Image' className='edit-btn'>
-                <BiEditAlt size={'15px'} fill='#fff' />
-              </button>
-            </div>
-          </div>
-        ) : (
-          <div className='user-image shadow'>
-            <div>
-              <img src={UserImage} alt='user-image' />
-              <button title='Upload Image' className='edit-btn'>
-                <BiEditAlt size={'15px'} fill='#fff' />
-              </button>
-            </div>
-          </div>
-        )
-      }
+      <UserProfileImageModal userId={userId}/>
+
       {userProfile ? (
-        <div className='user-detail-wrapper'>
+        <div className="user-detail-wrapper">
           <h3 className="text-uppercase my-1 fs-1 mx-0 text-center">
             User Details
           </h3>
 
-          <div className='row'>
+          <div className="row">
             <div className="col-md-6 mt-5">
-              <label htmlFor="userDetailsInputName" className='fs-2'>User Name:</label>
-              <p id='userDetailsInputName' className='fs-2 fw-light'>{userProfile.name}</p>
+              <label htmlFor="userDetailsInputName" className="fs-2">
+                User Name:
+              </label>
+              <p id="userDetailsInputName" className="fs-2 fw-light">
+                {userProfile.name}
+              </p>
             </div>
 
             <div className="col-md-6 mt-5">
-              <label htmlFor="userDetailsInputSurname" className='fs-2'>User Surname:</label>
-              <p id='userDetailsInputSurname' className='fs-2 fw-light'>{userProfile.surname}</p>
+              <label htmlFor="userDetailsInputSurname" className="fs-2">
+                User Surname:
+              </label>
+              <p id="userDetailsInputSurname" className="fs-2 fw-light">
+                {userProfile.surname}
+              </p>
             </div>
 
             <div className="col-md-6 mt-5">
-              <label htmlFor="userDetailsInputVillage" className='fs-2'>User Vilage:</label>
-              <p id='userDetailsInputVillage' className='fs-2 fw-light'>{userProfile.village}</p>
+              <label htmlFor="userDetailsInputVillage" className="fs-2">
+                User Vilage:
+              </label>
+              <p id="userDetailsInputVillage" className="fs-2 fw-light">
+                {userProfile.village}
+              </p>
             </div>
 
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputBirthDate" className="fs-2">
+                User BirthDate:
+              </label>
+              <p id="userDetailsInputBirthDate" className="fs-2 fw-light">
+                {userProfile.birthDate
+                  ? new Date(userProfile.birthDate).toDateString()
+                  : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputGender" className="fs-2">
+                User Gender:
+              </label>
+              <p id="userDetailsInputGender" className="fs-2 fw-light">
+                {userProfile.gender ? userProfile.gender : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputEducation" className="fs-2">
+                User Education:
+              </label>
+              <p id="userDetailsInputEducation" className="fs-2 fw-light">
+                {userProfile.education ? userProfile.education : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputOccupation" className="fs-2">
+                User Occupation:
+              </label>
+              <p id="userDetailsInputOccupation" className="fs-2 fw-light">
+                {userProfile.occupation ? userProfile.occupation : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputEmail" className="fs-2">
+                User Email:
+              </label>
+              <p id="userDetailsInputEmail" className="fs-2 fw-light">
+                {userProfile.email ? userProfile.email : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputCountryCode" className="fs-2">
+                User Country Code:
+              </label>
+              <p id="userDetailsInputCountryCode" className="fs-2 fw-light">
+                {userProfile.countryCode ? userProfile.countryCode : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputMobileNumber" className="fs-2">
+                User Mobile Number:
+              </label>
+              <p id="userDetailsInputMobileNumber" className="fs-2 fw-light">
+                {userProfile.mobileNumber ? userProfile.mobileNumber : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputMarriedStatus" className="fs-2">
+                User Married Status:
+              </label>
+              <p id="userDetailsInputMarriedStatus" className="fs-2 fw-light">
+                {userProfile.marriedStatus}
+              </p>
+            </div>
+
+            {userProfile.marriedStatus === "Married" ? (
+              <>
+                <div className="col-md-6 mt-5">
+                  <label htmlFor="userDetailsInputMarriedDate" className="fs-2">
+                    User Wedding Date:
+                  </label>
+                  <p id="userDetailsInputMarriedDate" className="fs-2 fw-light">
+                    {userProfile.weddingDate
+                      ? new Date(userProfile.weddingDate).toDateString()
+                      : "-"}
+                  </p>
+                </div>
+
+                {userProfile.gender === "MALE" ? (
+                  <div className="col-md-6 mt-5">
+                    <label
+                      htmlFor="userDetailsInputWifeSurname"
+                      className="fs-2"
+                    >
+                      User Wife Surname:
+                    </label>
+                    <p
+                      id="userDetailsInputWifeSurname"
+                      className="fs-2 fw-light"
+                    >
+                      {userProfile.wifeSurname ? userProfile.wifeSurname : "-"}
+                    </p>
+                  </div>
+                ) : null}
+              </>
+            ) : null}
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputFatherName" className="fs-2">
+                User Father Name:
+              </label>
+              <p id="userDetailsInputFatherName" className="fs-2 fw-light">
+                {userProfile.fatherName ? userProfile.fatherName : "-"}
+              </p>
+            </div>
+
+            <div className="col-md-6 mt-5">
+              <label htmlFor="userDetailsInputMotherName" className="fs-2">
+                User Mother Name:
+              </label>
+              <p id="userDetailsInputMotherName" className="fs-2 fw-light">
+                {userProfile.motherName ? userProfile.motherName : "-"}
+              </p>
+            </div>
           </div>
-
         </div>
       ) : (
         <div>
@@ -97,7 +217,7 @@ const UserDetails = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default UserDetails;
---
+++ frontend/src/admin/admin/user/UserProfileImageModal.jsx
@@ -0,0 +1,140 @@
+import axios from "axios";
+import React, { useRef, useState } from "react";
+import { EnvConfig } from "../../../config/env-config";
+
+const UserProfileImageModal = (props) => {
+  const [selectedFile, setselectedFile] = useState(null);
+  const [error, seterror] = useState(null);
+
+  const closeModalRef = useRef(null);
+
+  const handleSubmitForm = (e) => {
+    e.preventDefault();
+    if (error === null && selectedFile) {
+      console.log(selectedFile);
+
+      if (props.userId) {
+        console.log(props.userId);
+
+        let formData = new FormData();
+
+        formData.append("image", selectedFile);
+
+        let config = {
+          method: "post",
+          maxBodyLength: Infinity,
+          url: `${EnvConfig.LOCAL_URL}${EnvConfig.LOCAL_SUBURL}/user-profile/upload-image?userId=${props.userId}`,
+          headers: {},
+          data: formData,
+        };
+
+        axios.request(config)
+        .then((response) => {
+          if (response.data.status === 1) {
+            closeModalRef.current.click();
+          }
+          console.log(response.data);
+        })
+        .catch((error)=>{
+          console.log(error)
+        })
+      } else {
+        seterror("user id is not selected");
+      }
+    }
+  };
+
+  return (
+    <div className="modal fade" id="uploadProfileImageModal" tabIndex="-1">
+      <div className="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+        <div className="modal-content">
+          <div className="modal-header">
+            <button
+              type="button"
+              className="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+              ref={closeModalRef}
+              onClick={() => {
+                setselectedFile(null);
+                seterror(null)
+              }}
+            ></button>
+          </div>
+
+          <div className="modal-body d-flex flex-column align-items-center">
+            <form className="form-container" onSubmit={handleSubmitForm}>
+              <div className="upload-files-container">
+                <div className="drag-file-area">
+                  <span className="material-icons-outlined upload-icon">
+                    file upload
+                  </span>
+
+                  <label className="label">
+                    <span className="browse-files">
+                      <input
+                        type="file"
+                        className="default-file-input"
+                        name="image"
+                        onChange={(e) => {
+                          const file = e.target.files[0];
+
+                          if (file) {
+                            // Check if the file is an image file.
+                            if (!file.type.match(/^image\//)) {
+                              seterror("Please select an image file");
+                            } else {
+                              seterror(null);
+                              setselectedFile(file);
+                              console.log(file);
+                            }
+                          } else {
+                            setselectedFile(null);
+                            seterror("Please select an image file first");
+                          }
+                        }}
+                      />
+                      <span className="browse-files-text">Browse file </span>
+                      <span>from device</span>
+                    </span>
+                  </label>
+                </div>
+                {error !== null ? (
+                  <span className="cannot-upload-message">
+                    <span className="material-icons-outlined fs-4 fw-light text-danger">
+                      Error : {error}
+                    </span>
+                  </span>
+                ) : null}
+                {selectedFile ? (
+                  <div className="file-block">
+                    <div className="file-info">
+                      <span className="material-icons-outlined file-icon">
+                        description:
+                      </span>
+                      <span className="file-name fs-4">
+                        {" "}
+                        {selectedFile.name}
+                      </span>{" "}
+                      |
+                      <span className="file-size fs-4">
+                        {" "}
+                        {selectedFile.size}
+                      </span>
+                    </div>
+                  </div>
+                ) : null}
+
+                <button type="submit" className="upload-button">
+                  Upload
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default UserProfileImageModal;
