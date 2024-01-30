efffeb783495a9b1eac0e79434b77065089bfd0b
SutharUtsav <utsavsuthar24@gmail.com>
2024-01-30 23:52:10 +0530

    frontend: bind vales in event detail page

--- frontend/src/components/Events/Event.jsx
+++ frontend/src/components/Events/Event.jsx
@@ -3,6 +3,7 @@ import "./Events.css";
 import { Link, useParams } from "react-router-dom";
 import EventImg from "../../images/event.jpg";
 import { get } from "../../service/api-service";
+import { EnumMonths } from "../../consts/EnumMonths";
 
 const Event = () => {
   const params = useParams();
@@ -17,8 +18,8 @@ const Event = () => {
     eventDate : "1",
     eventMonth: "May",
     eventYear: "2024",
-    images: "",
-    mainImage: ""
+    images: {EventImg},
+    mainImage: {EventImg}
   }
 
   const [event, setevent] = useState(defaultEvent);
@@ -29,17 +30,25 @@ const Event = () => {
       get(`/event/${eventId}`)
       .then((response) => {
         if(response.data.status === 1){
-          // const localEvent = response.data.data
-          // const eventOn = new Date(response.data.data?.eventOn);
+          const localEvent = response.data.data
+          const eventOn = new Date(response.data.data?.eventOn);
           
-          // setevent({
-          //   ...event,
-          //   title: 
-          // })
+          setevent({
+            ...event,
+            title: localEvent.title,
+            description: localEvent.description,
+            eventDate: eventOn.getDate(),
+            eventMonth: EnumMonths[eventOn.getMonth()],
+            eventYear: eventOn.getFullYear(), 
+          })
+        }
+        else{
+          setevent(defaultEvent)
         }
       })
       .catch((error)=>{
         console.log(error)
+        setevent(defaultEvent)
       })
     }
   }, [eventId]);
@@ -49,11 +58,11 @@ const Event = () => {
       <div className="container">
         <div className="events-page-heading-container">
           <div className="container events-page-heading">
-            <h5>Social Gettogether at Samajwadi</h5>
+            <h5>{event.title}</h5>
             <div className="">
               <Link to="/">Home / </Link>
               <Link to="/events">Events</Link>
-              <span> / Social Gettogether</span>
+              <span> / {event.title}</span>
             </div>
           </div>
         </div>
@@ -63,23 +72,19 @@ const Event = () => {
             <div className="image-date-wrapper">
               <div className="date-container">
                 <div className="d-flex flex-column">
-                  <span>1</span>
-                  <span>Jun 2023</span>
+                  <span>{event.eventDate}</span>
+                  <span>{`${event.eventMonth} ${event.eventYear}`}</span>
                 </div>
               </div>
               <div className="main-image-container shadow">
-                <img src={EventImg} alt="event" />
+                <img src={EventImg} alt="event" loading="lazy"/>
               </div>
             </div>
 
             <div className="event-desc">
-              <h5 className="event-title">Social Gettogether at Samajwadi</h5>
+              <h5 className="event-title">{event.title}</h5>
               <p className="event-description">
-                The hyphenated term, get-together, is a noun that describes a
-                casual social gathering. The word get-together came into use at
-                the beginning of the twentieth century. Remember, the verb
-                phrase get together is never hyphenated, the noun form
-                get-together is always hyphenated.
+                {event.description}
               </p>
             </div>
 
